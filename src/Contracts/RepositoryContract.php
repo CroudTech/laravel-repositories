762c2d0d5ade0aa4a7a241120437f727fd61ee36
Jim Robinson <jim@croud.co.uk>
2017-03-10 13:50:57 +0000

    Add all basic methods and tests

--- src/Contracts/RepositoryContract.php
+++ src/Contracts/RepositoryContract.php
@@ -6,8 +6,8 @@ interface RepositoryContract
     public function all($columns = ['*']);
     public function create(array $data);
     public function delete($id);
-    public function find($id, $columns = ['*']);
-    public function findBy($field, $value, $columns = ['*']);
-    public function paginate($perPage = 20, $columns = ['*']);
-    public function update(array $data, $id);
+    public function find($id);
+    public function findBy($field, $value);
+    public function paginate($perPage = 20);
+    public function update($id, array $data);
 }
--- tests/BaseRepositoryTest.php
+++ tests/BaseRepositoryTest.php
@@ -6,6 +6,8 @@ use \Croud\RepositoryTests\Models\User as UserModel;
 use \Croud\RepositoryTests\Repositories\Contracts\UserRepositoryContract;
 use \Croud\RepositoryTests\Repositories\UserRepository;
 use \Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use \Illuminate\Pagination\AbstractPaginator as Paginator;
+use \Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BaseRepositoryTest extends TestCase
 {
@@ -94,5 +96,150 @@ class BaseRepositoryTest extends TestCase
 
         $repository->clearQuery();
         $this->assertEquals(10, $repository->all()->count());
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::create()
+     */
+    public function testCreateReturnsCorrectObjectInstance()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertInstanceOf(UserModel::class, $repository->create(['name' => 'Test Name']));
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::create()
+     */
+    public function testCreateReturnsObjectWithCorrectData()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertEquals('Test Name', $repository->create(['name' => 'Test Name'])->name);
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::create()
+     */
+    public function testCreateObjectExistsInTheDatabase()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $repository->create(['name' => 'Test Name 123']);
+        $this->assertInstanceOf(UserModel::class, UserModel::where('name', 'Test Name 123')->first());
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::find()
+     */
+    public function testFind()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $record = $repository->create(['name' => 'Test Name 123']);
+        $this->assertInstanceOf(UserModel::class, $found_record = $repository->find($record->id));
+        $this->assertEquals($record->name, $found_record->name);
+    }
+
+    /**
+     * @covers Croud\Repositories\BaseRepository::delete()
+     */
+    public function testDelete()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $record = $repository->create(['name' => 'Test Name 123']);
+        $this->assertTrue($repository->delete($record->id));
+        $this->assertNull($repository->find($record->id));
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::findBy()
+     */
+    public function testFindBy()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $record = $repository->create(['name' => 'Test Name 123']);
+        $this->assertInstanceOf(UserModel::class, $found_record = $repository->findBy('name', $record->name));
+        $this->assertEquals($record->name, $found_record->name);
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::update()
+     */
+    public function testUpdate()
+    {
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $record = $repository->create(['name' => 'Test Name 123']);
+        $this->assertTrue($repository->update($record->id, ['name' => 'Updated Name']));
+        $updated_record = $repository->find($record->id);
+        $this->assertEquals('Updated Name', $updated_record->name);
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::update()
+     */
+    public function testUpdateThrowsExceptionWhenNoRecordIsFound()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertTrue($repository->update(94321, ['name' => 'Updated Name']));
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::paginate()
+     */
+    public function testPaginateReturnsCorrectInstanceType()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertInstanceOf(Paginator::class, $repository->paginate(5));
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::paginate()
+     */
+    public function testPaginateReturnsCorrectNumberOfItems()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertEquals(5, $repository->paginate(5)->count());
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::paginate()
+     */
+    public function testPaginateUsesRepositoryQueryConstraints()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $repository->query()->where('name', 'Test User 1');
+        $this->assertEquals(1, $repository->paginate(10)->count());
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::simplePaginate()
+     */
+    public function testSimplePaginateReturnsCorrectInstanceType()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertInstanceOf(Paginator::class, $repository->simplePaginate(5));
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::simplePaginate()
+     */
+    public function testSimplePaginateReturnsCorrectNumberOfItems()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $this->assertEquals(5, $repository->simplePaginate(5)->count());
+    }
+
+    /**
+     * @covers \Croud\Repositories\BaseRepository::simplePaginate()
+     */
+    public function testSimplePaginateUsesRepositoryQueryConstraints()
+    {
+        $this->loadUserData();
+        $repository = $this->app->make(UserRepositoryContract::class);
+        $repository->query()->where('name', 'Test User 1');
+        $this->assertEquals(1, $repository->simplePaginate(10)->count());
     }
 }
